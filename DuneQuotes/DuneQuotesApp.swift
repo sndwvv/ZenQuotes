f131ab6a240a1d50fc4c40a0576c9be0505e7139
sndwvv <songparkhk@gmail.com>
2022-11-28 23:36:29 +0900

    Initial Commit

---
+++ DuneQuotes/DuneQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuneQuotesApp.swift
+//  DuneQuotes
+//
+//  Created by Songyee Park on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct DuneQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
